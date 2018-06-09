2d99c1c063341a7eb6a75d1e7e939fdf3974d011
Mark Lassiter <mlassiter@lexon-corp.com>
2018-06-09 13:33:24 -0400

    Completed demo.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  implements OnInit {
+
+  private readonly url = `${environment.employeeServiceUrl}/list`;
+
+  data;
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.data = null;
+    this.http.get(this.url, {})
+      .subscribe(data => {
+        console.log("Result:" + data);
+        this.data = data;
+      });
+  }
 }
+
+
